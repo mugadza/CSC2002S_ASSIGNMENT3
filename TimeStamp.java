0082e3da0ca2b0084f3f075eaa35c40320d2e8f8
mugadza <36105135+mugadza@users.noreply.github.com>
2019-09-09 09:22:35 +0200

    added features to timestamp

--- TimeStamp.java
+++ TimeStamp.java
@@ -16,6 +16,10 @@ public class TimeStamp{
 		this.windDetails[xIndex][yIndex] = windDetail;
 	}
 
+	public int size(){
+		return getYLength() * getXLength();
+	}
+
 	public int getYLength(){
 		if (windDetails.length > 0){
 			return this.windDetails[0].length;
@@ -28,43 +32,44 @@ public class TimeStamp{
 	}
 
 	public WindDetails at(int x, int y){
-			return windDetails[x][y];
-		
+		return windDetails[x][y];
 	}
 	
+	public WindDetails[][] getWindDetails(){
+		return windDetails;
+	}
+
 	public void classifier(){
 		WindDetails sumWindDetails = new WindDetails(0,0,0);
-		float total = 0;
 
 		for(int x = 0; x < this.windDetails.length; x++){
 			for(int y = 0; y < this.windDetails[0].length; y++){
 				sumWindDetails.addWindDetails(windDetails[x][y]);
-
 				
 				if (x-1 < windDetails.length &&  y-1 < windDetails[0].length && (x-1 >= 0) &&  (y-1 >= 0) ){
-					sumWindDetails = sumWindDetails.addWindDetails(windDetails[x-1][y-1] );
+					sumWindDetails.addWindDetails(windDetails[x-1][y-1] );
 				}
 				if (x-1 < windDetails.length &&  y < windDetails[0].length && (x-1 >= 0) &&  (y >= 0) ){
-					sumWindDetails = sumWindDetails.addWindDetails(windDetails[x-1][y] );
+					sumWindDetails.addWindDetails(windDetails[x-1][y] );
 				}
 				if (x-1 < windDetails.length &&  y+1 < windDetails[0].length && (x-1 >= 0) &&  (y+1 >= 0) ){
-					sumWindDetails = sumWindDetails.addWindDetails(windDetails[x-1][y+1] );
+					sumWindDetails.addWindDetails(windDetails[x-1][y+1] );
 				}
 				if (x < windDetails.length &&  y+1 < windDetails[0].length && (x >= 0) &&  (y+1 >= 0) ){
-					sumWindDetails = sumWindDetails.addWindDetails(windDetails[x][y+1] );
+					sumWindDetails.addWindDetails(windDetails[x][y+1] );
 				}
 				if (x < windDetails.length &&  y-1 < windDetails[0].length && (x >= 0) &&  (y-1 >= 0) ){
-					sumWindDetails = sumWindDetails.addWindDetails(windDetails[x][y-1] );
+					sumWindDetails.addWindDetails(windDetails[x][y-1] );
 				}
 				
 				if (x+1 < windDetails.length &&  y-1 < windDetails[0].length && (x+1 >= 0) &&  (y-1 >= 0) ){
-					sumWindDetails = sumWindDetails.addWindDetails(windDetails[x+1][y-1] );
+					sumWindDetails.addWindDetails(windDetails[x+1][y-1] );
 				}
 				if (x+1 < windDetails.length &&  y < windDetails[0].length && (x+1 >= 0) &&  (y >= 0) ){
-					sumWindDetails = sumWindDetails.addWindDetails(windDetails[x+1][y] );
+					sumWindDetails.addWindDetails(windDetails[x+1][y] );
 				}
 				if (x+1 < windDetails.length &&  y+1 < windDetails[0].length && (x+1 >= 0) &&  (y+1 >= 0) ){
-					sumWindDetails = sumWindDetails.addWindDetails(windDetails[x+1][y+1] );
+					sumWindDetails.addWindDetails(windDetails[x+1][y+1] );
 				}
 
 				float averageX = sumWindDetails.xAdvection /9;
@@ -76,13 +81,8 @@ public class TimeStamp{
 
 				windDetails[x][y].setCloudClassification(cloudType);
 			}
-		}
-
-
-				
+		}	
 	}
-
-
 
 	public double magnitudeValue (float x, float y){
 		float ans = (x*x) + (y*y);
@@ -101,8 +101,6 @@ public class TimeStamp{
 		 else{
 		 	return 2;
 		 }
-
-
 	}
 
 	@Override
@@ -112,7 +110,6 @@ public class TimeStamp{
 			for(int y = 0; y < this.windDetails[0].length; y++){
 				timeStampString += this.windDetails[x][y].toString();
 			}
-			// timeStampString += "\n";
 		}
 		return timeStampString;
 	}
